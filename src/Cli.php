96f5b4b0eae78a2f229272a37ac2606834c88653
Евгений Кадыков <jeka34volg@maail.ru>
2023-08-28 20:37:07 +0300

    Step number Three

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
